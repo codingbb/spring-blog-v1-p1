8c0c203a7fc805b3a38ea8a966ed0a95899da6af
SCHcoding <codingb007@gmail.com>
2024-02-06 11:57:30 +0900

    게시글 쓰기 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import shop.mtcoding.blog.user.User;
 
@@ -18,7 +19,31 @@ public class BoardController {
     private final HttpSession session;
     private final BoardRepository boardRepository;
 
-    @GetMapping({ "/", "/board" })
+    @PostMapping("/board/save")
+    public String save(BoardRequest.SaveDTO requestDTO, HttpServletRequest request) {
+        // 1. 인증 체크
+        User sessionUser = (User) session.getAttribute("sessionUser");
+        if (sessionUser == null) {
+            return "redirect:/loginForm";
+        }
+
+        // 2. 바디 데이터 확인 및 유효성 검사
+        System.out.println(requestDTO);
+
+        if (requestDTO.getTitle().length() > 30) {
+            request.setAttribute("status", 400);
+            request.setAttribute("msg", "title의 길이가 30자를 초과해서는 안돼요");
+            return "error/40x"; //BadRequest
+        }
+
+        // 3. 모델 위임
+        //insert into board_tb(title, content, user_id, created_at) value(?,?,?, now());
+        boardRepository.save(requestDTO, sessionUser.getId());
+
+        return "redirect:/";
+    }
+
+    @GetMapping({"/", "/board"})
     public String index(HttpServletRequest request) {
 
         List<Board> boardList = boardRepository.findAll();
@@ -44,7 +69,7 @@ public class BoardController {
 
     @GetMapping("/board/{id}")
     public String detail(@PathVariable int id, HttpServletRequest request) {
-        System.out.println("id : "+id);
+        System.out.println("id : " + id);
 
         // 바디 데이터가 없으면 유효성 검사가 필요없지 ㅎ
         BoardResponse.DetailDTO responseDTO = boardRepository.findById(id);
