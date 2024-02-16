17dadd5045036b4c595385c4a95d7ae79295029f
SCHcoding <codingb007@gmail.com>
2024-02-16 14:50:36 +0900

    페이징 번호없이 완료

--- src/main/java/shop/mtcoding/blog/board/BoardController.java
+++ src/main/java/shop/mtcoding/blog/board/BoardController.java
@@ -107,11 +107,27 @@ public class BoardController {
         return "redirect:/";
     }
 
-    @GetMapping({"/", "/board"})
-    public String index(HttpServletRequest request) {
+    //localhost:8080?page=1 -> page 값이 1
+    //localhost:8080 -> page 값이 0
+    @GetMapping("/")
+    public String index(HttpServletRequest request,
+                        @RequestParam(value = "page", defaultValue = "0") Integer page) {
 
-        List<Board> boardList = boardRepository.findAll();
+        List<Board> boardList = boardRepository.findAll(page);
+
+        // 전체 페이지 개수
+        int count = boardRepository.count().intValue();
+        // count가 6일때 전체는 2페이지
+        // 7 -> 3 page
+        // 5 -> 2 page
+        int namerge = count % 3 == 0 ? 0 : 1;
+        int allPageCount = count / 3 + namerge;
+
         request.setAttribute("boardList", boardList);
+        request.setAttribute("first", page==0);
+        request.setAttribute("last", allPageCount == page+1);
+        request.setAttribute("prev", page-1);
+        request.setAttribute("next", page+1);
 
         return "index";
     }
--- src/main/java/shop/mtcoding/blog/board/BoardRepository.java
+++ src/main/java/shop/mtcoding/blog/board/BoardRepository.java
@@ -23,8 +23,14 @@ public class BoardRepository {
         query.executeUpdate();
     }
 
-    public List<Board> findAll(){
-        Query query = em.createNativeQuery("select * from board_tb order by id desc", Board.class);
+    public Long count() {
+        Query query = em.createNativeQuery("select count(*) from board_tb");
+        return (Long)query.getSingleResult();
+    }
+
+    public List<Board> findAll(Integer page){
+        Query query = em.createNativeQuery("select * from board_tb order by id desc limit ?, 3", Board.class);
+        query.setParameter(1, page*3);
         return query.getResultList();
     }
 
